1eee93fb47ea254e13d66c2d6f4e967e98dada22
Jyoti Dhiman <36757634+jyotidhiman0610@users.noreply.github.com>
2019-08-16 19:06:02 +0530

    Update interleaved_strings.py

--- arrays/interleaved_strings.py
+++ arrays/interleaved_strings.py
@@ -14,3 +14,23 @@ def inter_util(A, B, C):
     
 def isInterleave(A, B, C):
     return inter_util(A, B, C)
+
+# Above solution has overlapping problems and the same has been handled in below code
+def inter_util(A, B, C, aa, bb, cc, a, b, c):
+    x = 0
+    y = 0
+    if (aa == a) & (bb == b) & (cc == c):
+        return 1
+    if aa < a:
+        if (A[aa] == C[cc]):
+            x = inter_util(A, B, C, aa+1, bb, cc+1, a, b ,c)
+    if (x == 0) & (bb < b):
+        if (B[bb] == C[cc]):
+            y = inter_util(A, B, C, aa, bb + 1, cc + 1, a, b, c)
+    return max(x, y)
+    
+def isInterleave(A, B, C):
+    a = len(A)
+    b = len(B)
+    c = len(C)
+    return inter_util(A, B, C, 0, 0, 0, a, b, c)
