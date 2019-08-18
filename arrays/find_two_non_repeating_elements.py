2d31cda8758b62f50720987aa3ab873f82054cd2
Jyoti Dhiman <36757634+jyotidhiman0610@users.noreply.github.com>
2019-08-18 15:15:44 +0530

    Create find_two_non_repeating_elements.py

---
+++ arrays/find_two_non_repeating_elements.py
@@ -0,0 +1,21 @@
+# https://practice.geeksforgeeks.org/problems/finding-the-numbers/0
+for _ in range(t):
+    n = int(input())
+    a = [int(i) for i in input().split(' ') if i!='']
+    res = 0
+    for element in a:
+        res ^= element
+    rightmost_set_bit = res & (~res + 1)
+    first_no = 0
+    second_no = 0
+    for element in a:
+        if element & rightmost_set_bit:
+            first_no ^= element
+        else:
+            second_no ^= element
+    if first_no < second_no:
+        print(first_no, second_no)
+    else:
+        print(second_no, first_no)
+
+    
