04a0cb3d614d369fb74dfab6050984148f66219c
Jyoti Dhiman <36757634+jyotidhiman0610@users.noreply.github.com>
2020-06-08 08:59:37 +0530

    Update coin_change.py

--- dp/coin_change.py
+++ dp/coin_change.py
@@ -1,18 +1,10 @@
-t = int(input())
-
-def calculate_change(arr, sum):
-    n = len(arr)
-    dp = [0 for _ in range(sum + 1)]
-    dp[0] = 1
-    for element in arr:
-        for s in range(sum + 1):
-            if element <= s:
-                dp[s] +=  dp[s - element]
-    
-    return dp[sum]
-
-for _ in range(t):
-    n = int(input())
-    arr = [int(i) for i in input().split(' ') if i!='']
-    sum = int(input())
-    print(calculate_change(arr, sum))
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0 for i in range(0, amount + 1)]
+        dp[0] = 1
+        
+        for coin in coins:
+            for amt in range(coin, amount+1):
+                dp[amt] += dp[amt - coin]
+        
+        return dp[-1]
